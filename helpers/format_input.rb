b363436d38c17e9ace5dca3b886d53aaee7859c0
The Combed Thunderclap <clappedthunder@gmail.com>
2015-08-01 18:23:49 +0100

    Permit each semicolon delimited search string to be specified as either absolute (with :abs) or only a regex substring of the search term

--- helpers/format_input.rb
+++ helpers/format_input.rb
@@ -3,18 +3,13 @@ require 'sinatra'
 class Web < Sinatra::Base
 
   def format_input(search_keys, search_terms)
-    if search_terms[-4..-1].eql? ':abs'
-      search_terms = search_terms[0..-5]
-      abs = true
-    else
-      abs = false
-    end
-
     search_hash = Hash[search_keys.split(',').zip(search_terms.split(';'))]
     search_hash.keys.each do |key|
       if search_hash["#{key}"] == nil
         raise "405"
-      elsif !abs
+      elsif search_hash["#{key}"][-4..-1].eql? ':abs'
+        search_hash["#{key}"] = search_hash["#{key}"][0..-5]
+      else
         search_hash["#{key}"] = /#{search_hash[key]}/
       end
     end
