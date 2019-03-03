57ef6dd98079d2aa7b2953622d0300e1315e0f68
The Combed Thunderclap <clappedthunder@gmail.com>
2019-03-03 18:03:51 +0000

    Use projection instead of fields property to select output fields

--- helpers/find_data.rb
+++ helpers/find_data.rb
@@ -2,10 +2,9 @@ require 'sinatra'
 
 class Web < Sinatra::Base
 
-  def new_find_data(search_hash, output_fields = { '_id' => 0, 'title' => 1, 'author' => 1, 
-                                                   'lines' => 1, 'linecount' => 1 })
+  def new_find_data(search_hash, output_fields = { '_id' => 0, 'title' => 1, 'author' => 1, 'lines' => 1, 'linecount' => 1 })
     settings.poetry_coll.find(
-      search_hash, :fields => output_fields
+      search_hash, { :projection => output_fields }
     ).to_a
   end
 
--- helpers/find_list.rb
+++ helpers/find_list.rb
@@ -4,7 +4,7 @@ class Web < Sinatra::Base
 
   def find_list(field)
     @field_data = settings.poetry_coll.find(
-      { "#{field}" => /./ }, :fields => { '_id' => 0, "#{field}" => 1 }
+      { "#{field}" => /./ }, :projection => { '_id' => 0, "#{field}" => 1 }
     ).sort_by {
       |poem| poem["#{field}"]
     }.uniq {
