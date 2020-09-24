b575b519904a9111d0b2aa4a94853a5f2bd4f5c7
The Combed Thunderclap <clappedthunder@gmail.com>
2020-09-24 13:29:03 +0100

    Make random field searches more granular to avoid mongodb cluster backend issue

--- app/helpers/find_random.rb
+++ app/helpers/find_random.rb
@@ -2,13 +2,14 @@ require 'sinatra'
 
 class Web < Sinatra::Base
 
-  def find_random()
+  def find_random(count, output_fields = { '_id' => 0, 'title' => 1, 'author' => 1, 'lines' => 1, 'linecount' => 1 })
+    @findings_data = []
     settings.poetry_coll.aggregate(
       [
-        { "$sample": { "size": 1} },
-        { "$project": { '_id' => 0, 'title' => 1, 'author' => 1, 'lines' => 1, 'linecount' => 1} }
+        { "$sample": { "size": count} },
+        { "$project": output_fields }
       ]
-    ).each { |i| @findings_data = i  }
+    ).each { |i| @findings_data.append(i)  }
     @findings_data
   end
 
