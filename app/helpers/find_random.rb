7ecae629d37b3b695fe4ab4fec007c869057ed49
The Combed Thunderclap <clappedthunder@gmail.com>
2020-09-28 11:20:37 +0100

    Use aggregation method for all sampling searches as it is more efficient

--- app/helpers/find_random.rb
+++ app/helpers/find_random.rb
@@ -2,10 +2,11 @@ require 'sinatra'
 
 class Web < Sinatra::Base
 
-  def find_random(count, output_fields = { '_id' => 0, 'title' => 1, 'author' => 1, 'lines' => 1, 'linecount' => 1 })
+  def find_random(count, search_hash = {}, output_fields = { '_id' => 0, 'title' => 1, 'author' => 1, 'lines' => 1, 'linecount' => 1 })
     @findings_data = []
     settings.poetry_coll.aggregate(
       [
+        { "$match": search_hash },
         { "$sample": { "size": count} },
         { "$project": output_fields }
       ]
